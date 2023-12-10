9b1b2bd5ec58c03bfb2661fd861d1fbb76571495
Mor Marzan <mormarzan@gmail.com>
2023-12-10 14:39:39 +0200

    Add basic header

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,14 +1,31 @@
 import { Home } from './cmps/Home.jsx'
 
+const { useState } = React
+
 export function App() {
+
+    const [page, setPage] = useState('home')
+
     return (
-        <section className="app">
-            <header className="app-header">
-                <h1>Miss Books</h1>
+        <section className="app main-layout">
+
+            <header className="app-header full main-layout">
+                <section>
+                    <h1>Miss Books</h1>
+                    <nav className="app-nav">
+                        <a onClick={() => setPage('home')} href="#">Home</a>
+                        {/* <a onClick={() => setPage('about')} href="#">About</a>
+                        <a onClick={() => setPage('books')} href="#">Books</a> */}
+                    </nav>
+                </section>
             </header>
-            <main class="container">
-                <Home />
+
+            <main>
+                {page === 'home' && <Home />}
+                {/* {page === 'about' && <About />}
+                {page === 'cars' && <CarIndex />} */}
             </main>
+
         </section>
     )
 }
