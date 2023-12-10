c778593e6fa3ce915625bd5c77629120b07dc994
Mor Marzan <mormarzan@gmail.com>
2023-12-10 20:20:44 +0200

    Add LongTxt cmp

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -1,11 +1,13 @@
+import { LongTxt } from "./LongTxt.jsx"
 
 export function BookPreview({ book }) {
-    const {id, title, description, listPrice, thumbnail} = book
+    const { id, title, description, listPrice, thumbnail } = book
     return (
         <article className="book-preview">
             <h2>{title.toUpperCase()}</h2>
-            <h4>{description}</h4>
-            <img src={thumbnail} alt={title + ' hardcover image'}  />
+            {/* <h4>{description}</h4> */}
+            <LongTxt txt={description} />
+            <img src={thumbnail} alt={title + ' hardcover image'} />
         </article>
     )
 }
---
+++ cmps/LongTxt.jsx
@@ -0,0 +1,31 @@
+import { utilService } from "../services/util.service.js"
+const { useState, useEffect } = React
+
+export function LongTxt({ txt, length = 10 }) {
+
+    const [showFulltxt, setShowFulltxt] = useState(false)
+
+    useEffect(() => {
+    }, [showFulltxt])
+    
+    const words = txt.split(/\s+/)
+    let excerpt = words.slice(0, length).join(' ')
+    
+    if (length > words.length) return txt
+    return (
+        <article className="long-txt">
+            {showFulltxt && 
+            <React.Fragment>
+                <p>{utilService.capitalizeFirstLetter(txt)}</p>
+                <button onClick={() => setShowFulltxt(false)}>Read Less</button>
+            </React.Fragment>
+            }
+            {!showFulltxt &&
+            <React.Fragment>
+                <p>{utilService.capitalizeFirstLetter(excerpt)}</p>
+                <button onClick={() => setShowFulltxt(true)}>Read More</button>
+            </React.Fragment>
+            }
+        </article>
+    )
+}
