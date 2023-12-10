e21267dd9bfbe86a32d80a88a72a1426abd72fdb
Mor Marzan <mormarzan@gmail.com>
2023-12-10 14:56:47 +0200

    Add pages cmps

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,3 +1,5 @@
+import { AboutUs } from './pages/AboutUs.jsx'
+import { BookIndex } from './pages/BookIndex.jsx'
 import { Home } from './pages/Home.jsx'
 
 const { useState } = React
@@ -14,16 +16,16 @@ export function App() {
                     <h1>Miss Books</h1>
                     <nav className="app-nav">
                         <a onClick={() => setPage('home')} href="#">Home</a>
-                        {/* <a onClick={() => setPage('about')} href="#">About</a>
-                        <a onClick={() => setPage('books')} href="#">Books</a> */}
+                        <a onClick={() => setPage('about')} href="#">About Us</a>
+                        <a onClick={() => setPage('books')} href="#">Books</a>
                     </nav>
                 </section>
             </header>
 
             <main>
                 {page === 'home' && <Home />}
-                {/* {page === 'about' && <About />}
-                {page === 'cars' && <CarIndex />} */}
+                {page === 'about' && <AboutUs />}
+                {page === 'books' && <BookIndex />}
             </main>
 
         </section>
